a95843557e5ce2a562e825a5ee45303312f49531
Rohit Aggarwal <rohitaggarwal376@gmail.com>
2020-07-22 19:38:24 +0530

    minute-change

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css'
 import Todos from './Todo'
 class App extends React.Component{
--- src/Todo.js
+++ src/Todo.js
@@ -1,88 +1,87 @@
-import React from 'react'
-import InputToDo from "./components/inputTodo"
-import ListTodo from "./components/listToDo"
-import FilterTodo from "./components/filterTodo"
-import './styles.css'
-export default class Todos extends React.Component{
-    constructor(){
-        super()
-        this.state = {
-            todos : [{
-                title : "Help Someone",
-                active  : 1,
-            }],
-            allTodos : [],
-        }
-    }
-    addTodo = (title)=>{
-        let todo = {
-            title,
-            active : 1
-        }
-        let newTodosArray = [...this.state.todos, todo]
-        this.setState({
-            todos : newTodosArray,
-            allTodos : newTodosArray
-        })
+import React from "react";
+import InputToDo from "./components/inputTodo";
+import ListTodo from "./components/listToDo";
+import FilterTodo from "./components/filterTodo";
+import "./styles.css";
+export default class Todos extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [],
+      allTodos: []
+    };
+  }
 
-    }
+  addTodo = title => {
+    let todo = {
+      title,
+      active: 1
+    };
+    let newTodosArray = [...this.state.todos, todo];
+    this.setState({
+      todos: newTodosArray,
+      allTodos: newTodosArray
+    });
+  };
 
-    
-    updateTodo = (todo) =>{
-        let todoObjectIndex = this.state.todos.indexOf(todo)
-        let previousArray = [...this.state.todos]
-        previousArray[todoObjectIndex].active = !previousArray[todoObjectIndex]
-        .active;
+  updateTodo = todo => {
+    let todoObjectIndex = this.state.todos.indexOf(todo);
+    let previousArray = [...this.state.todos];
+    previousArray[todoObjectIndex].active = !previousArray[todoObjectIndex]
+      .active;
+    this.setState({
+      todos: previousArray,
+      allTodos: previousArray
+    });
+  };
+
+  deleteTodo = todo => {
+    let todoObjectIndex = this.state.todos.indexOf(todo);
+    let previousArray = [...this.state.todos];
+    previousArray.splice(todoObjectIndex, 1);
+    this.setState({
+      todos: previousArray,
+      allTodos: previousArray
+    });
+  };
+
+  filterTodos = filter => {
+    switch (filter) {
+      case "Active":
+        let activeTodos = this.state.allTodos.filter(todo => todo.active);
         this.setState({
-            todos : previousArray,
-            allTodos : previousArray
-        })
-    }
-    deleteTodo = (todo) =>{
-        let todoObjectIndex = this.state.todos.indexOf(todo)
-        let previousArray = [...this.state.todos]
-        previousArray.splice(todoObjectIndex, 1);
+          todos: activeTodos
+        });
+        break;
+      case "Completed":
+        let completedTodos = this.state.allTodos.filter(todo => todo.active);
         this.setState({
-            todos : previousArray,
-            allTodos : previousArray
-        })
+          todos: completedTodos
+        });
+        break;
+      default:
+        this.setState({
+          todos: [...this.state.allTodos]
+        });
     }
-    filterTodos = (filter) =>{
-        switch(filter){
-            case "Active":
-                let activeTodos = this.state.allTodos.filter(todo => todo.active);
-                this.setState({
-                    todos : activeTodos
-                })
-                break;
-            case "Completed":
-                let completedTodos = this.state.allTodos.filter(todo => todo.active);
-                this.setState({
-                    todos : completedTodos
-                })
-                break; 
-            default :
-            this.setState({
-                todos : [...this.state.allTodos]
-            })
-        }
-    }
-    render(){
-        return(
-            <>
-            <h1>todos</h1>
-            <div className = "container">
-                <InputToDo addTodo = {this.addTodo}/>
-                <ListTodo 
-                items = {this.state.todos} 
-                deleteTodo = {this.deleteTodo} 
-                updateTodo = {this.updateTodo}/>
-                <FilterTodo 
-                filterButtons = {["All", "Active", "Completed"]}
-                filterTodos = {this.filterTodos}
-            />
-            </div>
-            </>
-        )
-    }
+  };
+  render() {
+    return (
+      <>
+        <h1>todos</h1>
+        <div className="container">
+          <InputToDo addTodo={this.addTodo} />
+          <ListTodo
+            items={this.state.todos}
+            deleteTodo={this.deleteTodo}
+            updateTodo={this.updateTodo}
+          />
+          <FilterTodo
+            filterButtons={["All", "Active", "Completed"]}
+            filterTodos={this.filterTodos}
+          />
+        </div>
+      </>
+    );
+  }
 }
